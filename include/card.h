79ee25caaa3cd75c0f1130adbd629fac2552a031
cypox <am_neggaz@esi.dz>
2022-05-05 02:03:27 +0200

    beatified card printing

--- include/card.h
+++ include/card.h
@@ -57,19 +57,19 @@ public:
   {
     switch(c._face)
     {
-      case 1: s << "A  "; break;
-      case 2: s << "2  "; break;
-      case 3: s << "3  "; break;
-      case 4: s << "4  "; break;
-      case 5: s << "5  "; break;
-      case 6: s << "6  "; break;
-      case 7: s << "7  "; break;
-      case 8: s << "8  "; break;
-      case 9: s << "9  "; break;
-      case 10: s << "10 "; break;
-      case 11: s << "J  "; break;
-      case 12: s << "Q  "; break;
-      case 13: s << "K  "; break;
+      case 1: s << "A"; break;
+      case 2: s << "2"; break;
+      case 3: s << "3"; break;
+      case 4: s << "4"; break;
+      case 5: s << "5"; break;
+      case 6: s << "6"; break;
+      case 7: s << "7"; break;
+      case 8: s << "8"; break;
+      case 9: s << "9"; break;
+      case 10: s << "T"; break;
+      case 11: s << "J"; break;
+      case 12: s << "Q"; break;
+      case 13: s << "K"; break;
       default: s << "Unknown"; break;
     }
 
