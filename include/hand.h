02ed9379d2c04bed4e22b3dd32d388f32c702feb
cypox <am_neggaz@esi.dz>
2022-05-05 03:03:49 +0200

    compute flush logic

--- include/hand.h
+++ include/hand.h
@@ -7,11 +7,19 @@ class hand {
 public:
   hand(card cards[7])
   {
+    bool is_flush = false;
+
+    short count[4] = {0, 0, 0, 0};
+    for (int i = 0 ; i < 7 ; ++ i)
+    {
+      is_flush = (++ count[cards[i]._suit]) >= 5;
+    }
     for (int i = 0 ; i < 7 ; ++ i) std::cout << cards[i] << " "; std::cout << std::endl;
     std::sort(cards, cards+7, [](const card& a, const card& b) {
       return a._face > b._face;
     });
     for (int i = 0 ; i < 7 ; ++ i) std::cout << cards[i] << " "; std::cout << std::endl;
+
     // check for royal flush
     // check for straight flush
     // check for four of a kind
